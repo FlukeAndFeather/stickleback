a825985caba633fee26484145c95a942805dddd2
Max Czapanskiy <maxczap@stanford.edu>
2022-06-03 16:28:30 -0700

    Add tests for extraction and repro with seed

--- tests/testextract.py
+++ tests/testextract.py
@@ -1,9 +1,9 @@
 import pandas as pd
-from pandas.testing import assert_frame_equal, assert_index_equal
+from pandas.testing import assert_index_equal
 import numpy as np
 from numpy.testing import assert_array_equal
 import unittest
-from stickleback.preprocessing import extract_all, extract_nested, sample_nonevents
+import stickleback.util as sb_util
 
 n = 20
 idx1 = pd.date_range(start="2020-01-01 00:00", freq="S", periods=n, tz="Etc/GMT+7")
@@ -22,12 +22,13 @@ events = {
     "d1": idx1[[2, 6]],
     "d2": idx2[[3, 8]]
 }
+rng = np.random.Generator(np.random.PCG64(1234))
 
 class ExtractTestCase(unittest.TestCase):
     
     def test_extract_all(self):
         winsz = 3
-        result = extract_all(sensors, nth=1, win_size=winsz)
+        result = sb_util.extract_all(sensors, nth=1, win_size=winsz)
 
         # Size and contents
         self.assertEqual(len(result["d1"]), n - winsz + 1)
@@ -41,15 +42,16 @@ class ExtractTestCase(unittest.TestCase):
 
     def test_extract_nested(self):
         idx = {"d1": idx1[[2, 4]], "d2": idx2[[3, 5]]}
-        result = extract_nested(sensors, idx, win_size=3)
+        result = sb_util.extract_nested(sensors, idx, win_size=3)
 
         for i in idx:
             assert_index_equal(result[i].index, idx[i])
 
     def test_sample_nonevents(self):
-        result1 = sample_nonevents(sensors, events, win_size=3, seed=0x1234)
-        result2 = sample_nonevents(sensors, events, win_size=3, seed=0x1234)
+        nonevents = sb_util.sample_nonevents(sensors, events, win_size=3, rng=rng)
 
-        for r in result1:
-            assert_frame_equal(result1[r], result2[r])
-        # TODO assert nonevent windows don't overlap event windows
+        # Verify nonevent windows don't overlap event windows
+        for id, windows in nonevents.items():
+            for e in events[id]:
+                # Assumes 1 s freq and win_size = 3
+                assert(np.all(np.abs(e - windows.index) > pd.Timedelta("3s")))
---
+++ tests/testseed.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import pandas.testing as pdtst
+import sktime.classification.interval_based
+import sktime.classification.compose
+from stickleback.stickleback import Stickleback
+import stickleback.data
+import stickleback.util
+import unittest
+
+# Load sample data
+sensors, events = stickleback.data.load_lunges()
+
+# Split into test and train (2 deployments each)
+train_deployids = list(sensors.keys())[0:2]
+test_deployids = list(sensors.keys())[2:4]
+sensors_train = {k: sensors[k] for k in train_deployids}
+sensors_test = {k: sensors[k] for k in test_deployids}
+events_train = {k: events[k] for k in train_deployids}
+events_test = {k: events[k] for k in test_deployids}
+
+# Define, fit, predict, assess
+def run_stickleback():
+    # Define model
+    # Supervised Time Series Forests ensembled across the columns of `sensors`
+    cols = sensors[list(sensors.keys())[0]].columns
+    tsc = sktime.classification.interval_based.SupervisedTimeSeriesForest(n_estimators=2,
+                                                                          random_state=4321)
+    stsf = sktime.classification.compose.ColumnEnsembleClassifier(
+        estimators = [('STSF_{}'.format(col),
+                      tsc,
+                      [i])
+                      for i, col in enumerate(cols)]
+    )
+
+    sb = Stickleback(
+        local_clf=stsf,
+        win_size=20,
+        tol=pd.Timedelta("5s"),
+        nth=20,
+        n_folds=2,
+        seed=1234
+    )
+
+    # Fit, predict, assess
+    sb.fit(sensors_train, events_train)
+    predictions = sb.predict(sensors_test)
+    outcomes = sb.assess(predictions, events_test)
+    
+    return predictions, outcomes
+
+# Run the Stickleback define-fit-predict-assess pipeline twice
+# They should be the same!
+pred1, out1 = run_stickleback()
+pred2, out2 = run_stickleback()
+
+class SeedTestCase(unittest.TestCase):
+    
+    def test_predictions(self):
+        id = 'bw180905-42'
+        # Assert local probability equality
+        pdtst.assert_series_equal(pred1[id][0], pred2[id][0])
+        # Assert event prediction equality
+        pdtst.assert_index_equal(pred1[id][1], pred2[id][1])
+        # Assert outcome equality
+        pdtst.assert_series_equal(out1[id], out2[id])
